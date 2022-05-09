920ad8f60a42c3ce2fadd0019763278f3f32cc3b
kaname-png <inmortaldragonxspace@outlook.com>
2022-05-09 12:03:44 -0600

    feat(listeners): added args missing error for commands

--- src/listeners/commands/error.ts
+++ src/listeners/commands/error.ts
@@ -1,4 +1,4 @@
-import { ArgumentError, CommandErrorPayload, CommandEvents, Listener, PieceContext, UserError } from '@kaname-png/revoltx';
+import { ArgumentError, CommandErrorPayload, CommandEvents, Identifiers, Listener, PieceContext, UserError } from '@kaname-png/revoltx';
 import type { ListenerOptions } from '@kaname-png/revoltx';
 
 export class ErrorCommand extends Listener {
@@ -7,14 +7,24 @@ export class ErrorCommand extends Listener {
 	}
 
 	public run(payload: CommandErrorPayload) {
-		const error = payload.error as UserError;
+		const error = payload.error as Error;
 		if (error instanceof ArgumentError) {
 			const prefix = this.container.client.fetchPrefix(payload.message);
-
 			return payload.message.reply(
 				[
 					`You have entered the argument **${error.argument.name}** incorrectly in the command **${payload.command.name}**:`,
 					`\`\`\`\n${error.message}\n\`\`\``,
+					`For more information on how to use the command, use the command **${prefix}cmd ${payload.command.name}**.`
+				].join('\n'),
+				false
+			);
+		}
+
+		if (error instanceof UserError && error.identifier === Identifiers.ArgsMissing) {
+			const prefix = this.container.client.fetchPrefix(payload.message);
+			return payload.message.reply(
+				[
+					`You did not enter the necessary arguments to be able to execute the command **${payload.command.name}**.`,
 					`For more information on how to use the command, use the command **${prefix}cmd ${payload.command.name}**.`
 				].join('\n'),
 				false
